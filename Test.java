ec560f9c83e31d1dec9d4c7b745a870fedc31079
Pulla-prakash <106310074+Pulla-prakash@users.noreply.github.com>
2025-05-12 15:38:56 +0200

    Update2 Test.java

--- Test.java
+++ Test.java
@@ -3,7 +3,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("**************");
 		System.out.println("Hello from GITHub");
-		System.out.println("Prepared by prakash and sai");		
+		System.out.println("Prepared by prakash and sai");
+		System.out.println("Hello Jenkins");
 		System.out.println("**************");
 	}
 
